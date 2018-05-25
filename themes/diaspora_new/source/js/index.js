eba0fd910caf02d07bcd17d68ab3e383b8e840b3
DmooSoCool <DmooSoCool@gmail.com>
2018-05-26 03:00:52 +0800

    [+] 完成首页、分页、helper

--- themes/diaspora_new/source/js/index.js
+++ themes/diaspora_new/source/js/index.js
@@ -1,7 +1,7 @@
 ;(function(window, undefined) {
   var init = function(){
     // 执行页面加载动画.
-    window._LOADING('#pageLoader',{spinner:0, timeToHide: 2000, bgColor:'#ffffff'});
+    window._LOADING('#pageLoader',{spinner:0, timeToHide: 1000, bgColor:'#ffffff'});
     replaceTopArchiveHeight();
     $(window).on('load', replaceTopArchiveHeight);
     $(window).on('resize', replaceTopArchiveHeight);
