bad5eff0a054ef7350abcaee20b5c3702500bb96
dmoosocool <dmoosocool@gmail.com>
2018-06-01 02:46:53 +0800

    [!] 修正样式, 完成阅读数 喜欢数 计数.

--- themes/diaspora_new/source/js/post.js
+++ themes/diaspora_new/source/js/post.js
@@ -1,67 +1,109 @@
 ;(function(window, undefined){
+  
+  var leanCloudInit = function() {
 
-  var leanCloudInit = function(){
-    var appId = 'bmBLlAnGqYMrkdSpw1HmjPiy-gzGzoHsz',
-    appKey = 'JYawwvAvz8dkzdYAu4qh1RnF';
-
-    var initValine = function (appId, appKey){
+    var pageID = decodeURIComponent(location.pathname);
+    /**
+     * 初始化valine插件.
+     *
+     * @returns {void}
+     */
+    var initValine = function() {
       new Valine({
         el: '#comment' ,
         notify:false, 
         verify:false, 
-        appId: appId,
-        appKey: appKey,
-        placeholder: 'just go go',
-        path:window.location.pathname, 
+        appId: 'bmBLlAnGqYMrkdSpw1HmjPiy-gzGzoHsz',
+        appKey: 'JYawwvAvz8dkzdYAu4qh1RnF',
+        placeholder: '快来加入评论吧~ (≧∇≦)ﾉ',
+        path: pageID,
         avatar:'mm' 
       });
     };
 
-    // 累计
-    var addCount = function(type) {
-      var Counter = AV.Object.extend('Counter');
-      var counter = new AV.Query(Counter);
-      var type = type || 'read';
-        
-      counter.equalTo('url', location.pathname);
-      counter.find().then(function(resp) {
+    /**
+     * 根据类型计数.
+     * 
+     * @param {string} type 添加计数的类型　'read'|'like'
+     * @param {function} callback 添加完成回调.
+     */
+    var addCount = function(type, callback) {
+      var Counter = AV.Object.extend('Counter'),
+        counter = new AV.Query(Counter),
+        type = type || 'read';
+
+      // 查询当前文章的数据.
+      counter.equalTo('url', pageID);
+      // 取出数据.
+      counter.first().then(function(resp) {
+        // 有数据则累加
         if(!!resp){
           resp.increment(type, 1);
-          resp.save({fetchWhenSave: true});
+          resp.save({fetchWhenSave: true}).then(function(){
+            callback && callback({status: 1});
+          });
         }
+        // 无数据则插入该文章的初始数据.
         else{
           var newCounter = new Counter();
-          newCounter.set('url', location.pathname);
+          newCounter.set('url', pageID);
           newCounter.set('read', 1);
           newCounter.set('like', 0);
-          newCounter.save({fetchWhenSave: true});
+          newCounter.save({fetchWhenSave: true}).then(function(){
+            callback && callback({status: 1});
+          });
         }
       }, function(error) {
-        console.log('err:',error);
+        callback && callback({status: 0, error: error});
       })
     };
 
-    var getCount = function(type, callback) {
+    /**
+     * 获取计数
+     * 
+     * @param {function} callback 
+     */
+    var getCount = function(callback) {
       var Counter = AV.Object.extend('Counter');
       var counter = new AV.Query(Counter);
-      counter.equalTo('url', location.pathname);
-      counter.find().then(function(resp){
-        callback && callback(resp.get(type));
+      // 查询当前文章的数据
+      counter.equalTo('url', pageID);
+      counter.first().then(function(resp) {
+        callback && callback(resp);
       });
     };
+
+    /**
+     * 获取评论数
+     * 
+     * @param {function} callback 回调函数
+     * @returns {Promise}
+     */
+    var getCommentCount = function(callback) {
+      var Comment = AV.Object.extend('Comment');
+      var query = new AV.Query(Comment);
+
+      query.equalTo('url', pageID);
+      return query.count();
+    }
+
     // 初始化Valine评论.
-    initValine(appId, appKey);
+    initValine();
 
-    // 阅读数递增.
-    addCount();
-
-    getCount('read', function(count) {
-      $('.describe .reader em').text(count);
+    // 先添加文章阅读数然后拿到数据,
+    addCount('read', function(status, err){
+      if(status){
+        getCount(function(resp) {
+          getCommentCount().then( function(comment) {
+            $('.describe .comment em').text(comment);
+            $('.describe .reader em').text(resp.get('read'));
+            $('.describe .likes em').text(resp.get('like'));
+          });
+        });
+      }else{
+        console.log(err);
+      }
     });
-
-    getCount('like', function(count){
-      $('.describe .likes em').text(count);
-    })
   }
 
   var init = function(){
